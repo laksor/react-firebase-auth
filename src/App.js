08546d3ce58fdcc61b7ffc8a3fcf6040addbc54c
laskor <al.moksud@gmail.com>
2022-04-12 01:57:56 -0400

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import './App.css';
+import "./App.css";
 import { getAuth } from "firebase/auth";
 import app from "./firebase.init";
 
 const auth = getAuth(app);
 
 function App() {
+  const handleEmail = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handlePass = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    console.log('form submited');
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-    <form>
-    <input type="text"></input>
-    <input type="password"></input>
-    </form>
-      
+      <form onSubmit={handleSubmit}>
+        <input onBlur={handleEmail} type="email"></input>
+        <br />
+        <input onBlur={handlePass} type="password"></input>
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
