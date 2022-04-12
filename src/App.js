9b2a57ff70f5ff2df64a22853bade8d1099e22c7
laskor <al.moksud@gmail.com>
2022-04-12 19:26:13 -0400

    email, pass

--- src/App.js
+++ src/App.js
@@ -1,33 +1,88 @@
 import "./App.css";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "./firebase.init";
 import { Button, Container, Form, Row } from "react-bootstrap";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
+  const [error, setError] = useState();
+  const [registered, setRegistered] = useState();
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleEmail = (event) => {
-    console.log(event.target.value);
+    setEmail(event.target.value);
   };
 
   const handlePass = (event) => {
-    console.log(event.target.value);
+    setPassword(event.target.value);
   };
 
+  const handleCheckbox = (event) =>{
+    setRegistered(event.target.checked);
+  }
+
   const handleSubmit = (event) => {
-    console.log("form submited");
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+
+    if(registered){
+      signInWithEmailAndPassword(auth, email, password)
+      .then((result)=>{
+        const user = result.user;
+        console.log(user);
+        setEmail('');
+        setPassword('');
+      })
+      .catch((error) =>{
+        console.log(error);
+        setError(error.message);
+      })
+    }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setEmail('');
+        setPassword('');
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      });
+    }
     event.preventDefault();
-  };
+  }
+
+  const verifyEmail = () =>{
+    
+  }
 
   return (
     <div>
       <Container className="shadow-lg p-5 mt-5 rounded">
         <Row className="w-50 mx-auto">
-        <h2 className="mt-3 text-danger">Please Login</h2>
-          <Form onSubmit={handleSubmit}>
+          <h2 className="mt-3 text-danger">Please {registered ? 'login' : 'Register'}</h2>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group className="mb-3 mt-2" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" />
+              <Form.Control
+                onBlur={handleEmail}
+                type="email"
+                placeholder="Enter email"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid email.
+              </Form.Control.Feedback>
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -35,10 +90,28 @@ function App() {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control onBlur={handlePass} type="password" placeholder="Password" />
+              <Form.Control
+                onBlur={handlePass}
+                type="password"
+                placeholder="Password"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid password.
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Check
+                onChange={handleCheckbox}
+                type="checkbox"
+                label="Already Registered ?"
+                feedback="You must check before submitting."
+                feedbackType="invalid"
+              />
             </Form.Group>
             <Button className="rounded-pill" variant="danger" type="submit">
-              Submit
+              {registered ? 'Login' : 'Register'}
             </Button>
           </Form>
         </Row>
