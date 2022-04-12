711f88edb2b9766c4032c71de455cbcac2dcb10d
laskor <al.moksud@gmail.com>
2022-04-12 01:41:07 -0400

    input type added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth } from "firebase/auth";
+import app from "./firebase.init";
+
+const auth = getAuth(app);
 
 function App() {
   return (
     <div className="App">
+    <form>
+    <input type="text"></input>
+    <input type="password"></input>
+    </form>
       
     </div>
   );
