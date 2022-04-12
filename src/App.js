32ec0c0c219766e2d1e7a7ca11210ce003eded94
laskor <al.moksud@gmail.com>
2022-04-12 02:11:47 -0400

    form added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { getAuth } from "firebase/auth";
 import app from "./firebase.init";
+import { Button, Container, Form, Row } from "react-bootstrap";
 
 const auth = getAuth(app);
 
@@ -14,19 +15,34 @@ function App() {
   };
 
   const handleSubmit = (event) => {
-    console.log('form submited');
+    console.log("form submited");
     event.preventDefault();
-  }
+  };
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input onBlur={handleEmail} type="email"></input>
-        <br />
-        <input onBlur={handlePass} type="password"></input>
-        <br />
-        <button type="submit">Login</button>
-      </form>
+    <div>
+      <Container className="shadow-lg p-5 mt-5 rounded">
+        <Row className="w-50 mx-auto">
+        <h2 className="mt-3 text-danger">Please Login</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3 mt-2" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control onBlur={handlePass} type="password" placeholder="Password" />
+            </Form.Group>
+            <Button className="rounded-pill" variant="danger" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Row>
+      </Container>
     </div>
   );
 }
