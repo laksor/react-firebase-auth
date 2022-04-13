90ab60a07229a0ec368d03ea4121e2d4da5886aa
laskor <al.moksud@gmail.com>
2022-04-12 21:57:02 -0400

    user name updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "./firebase.init";
 import { Button, Container, Form, Row } from "react-bootstrap";
 import { useState } from "react";
@@ -7,11 +13,16 @@ import { useState } from "react";
 const auth = getAuth(app);
 
 function App() {
-  const [error, setError] = useState();
-  const [registered, setRegistered] = useState();
+  const [error, setError] = useState('');
+  const [registered, setRegistered] = useState(false);
   const [validated, setValidated] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleName = (event) =>{
+    setName(event.target.value);
+  }
 
   const handleEmail = (event) => {
     setEmail(event.target.value);
@@ -21,9 +32,9 @@ function App() {
     setPassword(event.target.value);
   };
 
-  const handleCheckbox = (event) =>{
+  const handleCheckbox = (event) => {
     setRegistered(event.target.checked);
-  }
+  };
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -33,56 +44,76 @@ function App() {
 
     setValidated(true);
 
-    if(registered){
+    if (registered) {
       signInWithEmailAndPassword(auth, email, password)
-      .then((result)=>{
-        const user = result.user;
-        console.log(user);
-        setEmail('');
-        setPassword('');
-      })
-      .catch((error) =>{
-        console.log(error);
-        setError(error.message);
-      })
-    }
-    else{
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+          setEmail("");
+          setPassword("");
+        })
+        .catch((error) => {
+          console.log(error);
+          setError(error.message);
+        });
+    } else {
       createUserWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        const user = result.user;
-        console.log(user);
-        setEmail('');
-        setPassword('');
-        verifyEmail();
-      })
-      .catch((error) => {
-        console.log(error);
-        setError(error.message);
-      });
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+          setEmail("");
+          setPassword("");
+          verifyEmail();
+        })
+        .catch((error) => {
+          console.log(error);
+          setError(error.message);
+        });
     }
     event.preventDefault();
-  }
+  };
 
-   const passReset = () =>{
-     sendPasswordResetEmail(auth, email)
-     .then(() =>{
-       console.log('password reset');
-     })
-   }
+  const passReset = () => {
+    sendPasswordResetEmail(auth, email).then(() => {
+      console.log("password reset");
+    });
+  };
 
-  const verifyEmail = () =>{
-    sendEmailVerification(auth.currentUser)
-    .then( () =>{
-      console.log('email verify');
-    })
-  }
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      console.log("email verify");
+    });
+  };
 
   return (
     <div>
       <Container className="shadow-lg p-5 mt-5 rounded">
         <Row className="w-50 mx-auto">
-          <h2 className="mt-3 text-danger">Please {registered ? 'login' : 'Register'}</h2>
+          <h2 className="mt-3 text-danger">
+            Please {registered ? "login" : "Register"}
+          </h2>
+
+          <Form.Group className="mb-2 mt-2">
+              <Form.Check
+                onChange={handleCheckbox}
+                type="checkbox"
+                label="Already Registered ?"
+                feedback="You must check before submitting."
+                feedbackType="invalid"
+              />
+            </Form.Group>
+
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            {!registered && <Form.Group className="mb-3 mt-2" controlId="formBasicEmail">
+              <Form.Label>Your Name</Form.Label>
+              <Form.Control
+                onBlur={handleName}
+                type="text"
+                placeholder="Enter name"
+                required
+              />
+            </Form.Group>}
+
             <Form.Group className="mb-3 mt-2" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control
@@ -112,19 +143,14 @@ function App() {
               </Form.Control.Feedback>
             </Form.Group>
 
-            <Form.Group className="mb-3">
-              <Form.Check
-                onChange={handleCheckbox}
-                type="checkbox"
-                label="Already Registered ?"
-                feedback="You must check before submitting."
-                feedbackType="invalid"
-              />
-            </Form.Group>
-            <Button onClick={passReset} variant="link">forget password ? </Button>
+            <p className="text-danger">{error}</p>
+
+            <Button onClick={passReset} variant="link">
+              forget password ?{" "}
+            </Button>
             <br />
             <Button className="rounded-pill" variant="danger" type="submit">
-              {registered ? 'Login' : 'Register'}
+              {registered ? "Login" : "Register"}
             </Button>
           </Form>
         </Row>
